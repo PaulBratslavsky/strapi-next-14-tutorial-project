ec9da64734d027adddbda7e18671fb66845aa367
Paul Bratslavsky <paulbratslavsky@Pauls-MacBook-Pro.local>
2024-02-15 14:22:33 -0600

    profile link

--- frontend/components/custom/LoggedIn.tsx
+++ frontend/components/custom/LoggedIn.tsx
@@ -1,6 +1,7 @@
 import { getUserMeLoader } from "@/data/services/get-user-me-loader";
 import { logoutAction } from "@/data/actions/auth-actions";
 import { LogoutIcon } from "@/components/icons/LogoutIcon";
+import Link from "next/link";
 
 interface AuthUserProps {
   username: string;
@@ -8,17 +9,24 @@ interface AuthUserProps {
 }
 
 function LogoutButton() {
-  return <form action={logoutAction}>
-    <button type="submit">
-      <LogoutIcon className="w-6 h-6 hover:text-primary" />
-    </button>
-  </form>;
+  return (
+    <form action={logoutAction}>
+      <button type="submit">
+        <LogoutIcon className="w-6 h-6 hover:text-primary" />
+      </button>
+    </form>
+  );
 }
 
 function AuthUser({ userData }: { readonly userData: AuthUserProps }) {
   return (
     <div className="flex gap-2">
-      <h1 className="font-semibold">{userData.username}</h1>
+      <Link
+        href="/dashboard/account"
+        className="font-semibold hover:text-primary"
+      >
+        {userData.username}
+      </Link>
       <LogoutButton />
     </div>
   );
@@ -30,6 +38,5 @@ export async function LoggedIn({
   readonly children: React.ReactNode;
 }) {
   const user = await getUserMeLoader();
-  console.log("user", user);
   return <div>{user.ok ? <AuthUser userData={user.data} /> : children}</div>;
 }

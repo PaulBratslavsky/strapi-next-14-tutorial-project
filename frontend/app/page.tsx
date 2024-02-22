efb027e125c900572fd625dcf6ce0bb5de215c58
Paul Bratslavsky <Paul.bratslavsky@gmail.com>
2024-02-21 18:58:54 -0600

    added info credit box

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,7 @@
 import BenefitsSection from "@/components/custom/BenefitsSection";
 import HeroSection from "@/components/custom/HeroSection";
 import { Fragment } from "react";
-
+// Update to this ui https://www.figma.com/file/AKLjfLDR3l1V8OUtkDUjua/7-%2F-Feb-21--?type=design&node-id=0-1&mode=design&t=lMH6o5LBoyfjR88x-0
 export default function HomeRoute() {
   return (
     <Fragment>
--- frontend/components/custom/forms/ProfileForm.tsx
+++ frontend/components/custom/forms/ProfileForm.tsx
@@ -29,7 +29,7 @@ function CountBox({ text }: { readonly text: number }) {
   const color = text > 0 ? "text-primary" : "text-red-500";
   return (
     <div className="flex items-center justify-center h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none">
-      You have<span className={cn(style, color)}>{text}</span>credits
+      You have<span className={cn(style, color)}>{text}</span>credit(s)
     </div>
   );
 }

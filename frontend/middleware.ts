4f6e8b48bb5eee6305de7a768db7cfe01f635266
Paul Bratslavsky <Paul.bratslavsky@gmail.com>
2024-02-21 20:09:05 -0600

    why

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -6,6 +6,10 @@ export async function middleware(request: NextRequest) {
   const user = await  getUserMeLoader();
   const currentPath = request.nextUrl.pathname;
 
+  console.log("user", user);
+  console.log("currentPath", currentPath);
+  console.log("request.url", request.url);
+
   if (currentPath.startsWith("/dashboard") && user.ok === false) {
     return NextResponse.redirect(new URL("/login", request.url));
   }

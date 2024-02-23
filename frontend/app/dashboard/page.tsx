195556925c6ce05a50aab3b54853183954b1c424
Paul Bratslavsky <paulbratslavsky@Pauls-MacBook-Pro.local>
2024-02-23 12:30:06 -0600

    update

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -1,3 +1,43 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { MountainIcon } from "@/components/icons/MountainIcon";
+
 export default function DashboardIndexRoute() {
-  return <h1>Dashboard</h1>
+  return (
+    <div className="grid gap-4 md:grid-cols-3 lg:grid-cols-3 p-4">
+      <Card>
+        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+          <CardTitle className="text-sm font-medium">Summaries</CardTitle>
+          <MountainIcon className="h-4 w-4 text-muted-foreground" />
+        </CardHeader>
+        <CardContent>
+          <div className="text-2xl font-bold">12</div>
+          <p className="text-xs text-muted-foreground">
+            Checkout all your summaries
+          </p>
+        </CardContent>
+      </Card>
+      <Card>
+        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+          <CardTitle className="text-sm font-medium">Credits</CardTitle>
+          <MountainIcon className="h-4 w-4 text-muted-foreground" />
+        </CardHeader>
+        <CardContent>
+          <div className="text-2xl font-bold">10</div>
+          <p className="text-xs text-muted-foreground">All your remaining credits</p>
+        </CardContent>
+      </Card>
+      <Card>
+        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+          <CardTitle className="text-sm font-medium">Notes</CardTitle>
+          <MountainIcon className="h-4 w-4 text-muted-foreground" />
+        </CardHeader>
+        <CardContent>
+          <div className="text-2xl font-bold">24</div>
+          <p className="text-xs text-muted-foreground">
+            Checkout all your notes
+          </p>
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
